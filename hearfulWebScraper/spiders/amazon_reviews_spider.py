8a117679145fc3fc40e07abc2ca77f226fe84e34
Christopher Sannar <chris.sannar.dev@gmail.com>
2021-02-15 15:35:03 -0700

    Made a request to a few items, set up dicts and classes to keep track of things, and added headers to access Amazon
    So amazon is good at blocking bots, so we're gonna have to follow the rules in order to maintain access. Added some header information that we'll have to randomly change in order to not get blocked again. In the meantime, we'll have to be sparing with the requests else our IP will get banned. Also added some dicts with distinct information like the selectors on the page for the necessary information as well as a class to have our pulled data be represented as an 'AmazonItem' object. From there I'll expand it out and give the class more methods like adding and organizing reviews and simply a 'save to database' kind of thing.

--- hearfulWebScraper/spiders/amazon_reviews_spider.py
+++ hearfulWebScraper/spiders/amazon_reviews_spider.py
@@ -1,5 +1,17 @@
 import scrapy
 
+CSSSelectors = {
+  'product_title' : "#productTitle::text",
+  'product_brand' : "#productOverview_feature_div", # tr:first-child td:last-child span",
+  'review_count' : "#acrCustomerReviewText::text",
+}
+
+XPathSelectors = {
+  'product_brand': '//div[@id="productOverview_feature_div"]'
+}
+
+headers = {'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"}
+
 # Grab this list of sites to crawl
 site_list_path = "./site_list.txt"
 def get_sites():
@@ -15,10 +27,47 @@ class AmazonReviewsSpider(scrapy.Spider):
     # Get the list of sites to crawl and parse the data for each
     urls = get_sites()
     for url in urls:
-      yield scrapy.Request(url=url, callback=self.initialParse)
+      yield scrapy.Request(url=url, headers=headers, callback=self.initialParse)
   
   # Parses the main site page
   def initialParse(self, response):
-    for rating in response.css('#acrCustomerReviewText::text'):
-      print(rating)
+    productName = str.strip(response.css(CSSSelectors['product_title']).get())
+    # productBrand = str.strip(response.css(CSSSelectors['product_brand']).get())
+    reviewCount = int(response.css(CSSSelectors['review_count']).get().split(' ', 1)[0])
     
+
+    print("-----------------------------------------------")
+    print(response.xpath(XPathSelectors['product_brand']).get())
+    print({
+      reviewCount,
+      # productBrand,
+      productName,
+    })
+    print("-----------------------------------------------")
+
+# A single amazon item we are reviewing
+class AmazonItem:
+
+  def __init__(self, name="", brand="", source="", price=0.0, rating=0.0, reviews=[]):
+    self._name = name
+    self._brand = brand
+    self._source = source
+    self._price = price
+    self._rating = rating
+    self._reviews = reviews
+
+  # Follow Encapsulation
+  def getName(self):
+    return self._name
+
+  def getBrand(self):
+    return self._brand
+
+  def getSource(self):
+    return self._source
+
+  def getPrice(self):
+    return self._price
+
+  def getRating(self):
+    return self._rating
--- notes.md
+++ notes.md
@@ -8,7 +8,8 @@
 
 - Had some issues with installing pip on my machine. Mostly because I was trying to use python2 to run python3. #Was able to install it all using a script file `get-pip.py` and that got it running. Also had to update the pathing to run pip since it wasn't installed in the proper directory (I'm using WSL so there are some issues there)
 - It's `yield`, not `yeild`
-- 
+- Amazon temporarily allowed access to python bots, but eventually blocked us. That was fixed by setting a different user agent, however we'll have to changed it every now and then to prevent Amazon from figuring out we're using a bot
+  * 'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"
 
 ## Web Structure
 
@@ -26,3 +27,4 @@ Useful Selectors
 - Commands:
   * `scrapy crawl <spider>` - Runs the following spider
   * `scrapy crawl <spider> -o <file>` - Sends the Output to the given file
+  * `scrapy shell -s USER_AGENT='<agent>' '<url>' - Sets the shell for the request
